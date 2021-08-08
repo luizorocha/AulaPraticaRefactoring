1f687716d9267e72afd634b17f59c987b6094135
luizorocha <82480022+luizorocha@users.noreply.github.com>
2021-08-08 02:26:03 +0000

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -3,7 +3,7 @@ import java.util.Enumeration;
 public class HtmlStatement extends Statement {
   public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
+      String result = "<H1>Rental Record for <EM>" + aCustomer.getName() +
       "</EM></H1><P>\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
@@ -18,5 +18,5 @@ public class HtmlStatement extends Statement {
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
       "</EM> frequent renter points<P>";
       return result;
-  }
+   }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,7 +1,7 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-  public String value(Customer aCustomer) {
+ public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
       String result = "Rental Record for " + aCustomer.getName() +
       "\n";
@@ -12,12 +12,11 @@ public class TextStatement extends Statement {
          String.valueOf(each.getCharge()) + "\n";
       }
       //add footer lines
-      result += "Amount owed is " +
+      result += "You owe " +
       String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
+      result += "On this rental you earned " +
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
       " frequent renter points";
       return result;
    }
-
 }
